065cd9a5724d26115388a8ce104ad2db5c4605c3
shoaib900 <rshoaib376@gmail.com>
2024-08-17 12:31:47 +0500

    update js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+// alert("hello world")
+
+        // 17 DOM id
+
+        // var rgn = Math.round( Math.random() * 10)
+        // console.log(rgn);
+        // function s(){
+        //     var input = parseInt( prompt("Enter the number") )
+            
+        //     if( input === rgn){
+        //         var b = document.getElementById("demo")
+        //         b.style.backgroundColor = "red"
+        //         document.getElementById("text").innerHTML = " your answer is right"
+        //     }else{
+        //         var b = document.getElementById("demo")
+        //         b.style.backgroundColor = "black"
+        //         document.getElementById("text").innerHTML = "<h1>your answer is wrong </h1>"
+        //           document.getElementById("text").style.color = "red"
+        //     }
+        // }
+
+
+        // 18 objects
+        // 19 destucture Objects
+        // 20 Date Objects
+        // 21 exceptional handling
+        // 22 Debugger
+        // 23 setTimeout / setInterval
+        // 24 callback methods
+        // 25 Promises
+        // 26 async / await
+        // 27 class / OOP
+        // 28 DOM Manipulations
